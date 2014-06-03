4202ee41db3e9133c833bf639b0b24e9d6aaf64c
Kevin Markham <justmarkham@gmail.com>
2014-06-03 00:13:00 -0400

    add more comments to plot4.R

--- plot4.R
+++ plot4.R
@@ -17,15 +17,19 @@ power$DateTime <- strptime(datetime, "%d/%m/%Y %H:%M:%S")
 # Plot 4
 png(file="plot4.png")
 par(mfrow=c(2,2))
+# upper left plot:
 plot(power$DateTime, power$Global_active_power, type="l", xlab="",
      ylab="Global Active Power")
+# upper right plot:
 plot(power$DateTime, power$Voltage, type="l", xlab="datetime", ylab="Voltage")
+# lower left plot:
 plot(power$DateTime, power$Sub_metering_1, type="l", xlab="",
      ylab="Energy sub metering")
 lines(power$DateTime, power$Sub_metering_2, col="red")
 lines(power$DateTime, power$Sub_metering_3, col="blue")
 legend("topright", lty=1, bty="n", col=c("black","red","blue"),
        legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+# lower right plot:
 plot(power$DateTime, power$Global_reactive_power, type="l", xlab="datetime",
      ylab="Global_reactive_power")
 dev.off()
